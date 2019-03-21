7d1618f2fb59452b316dd7486171b525ec4246e9
epsilonGtMyon <epsilon.87gmaigt@gmail.com>
2019-03-21 15:56:08 +0900

    ツールチップというのかな？でも入力もできる

---
+++ src/pages/sandbox01/PseudoTooltip.vue
@@ -0,0 +1,28 @@
+<template>
+  <span class="preudotooltip">
+    <slot></slot>
+    <div class="tooltip-area">
+      <slot name="tooltip-content"></slot>
+    </div>
+  </span>
+</template>
+
+<script>
+//なんちゃってツールチップ
+export default {
+  name: "PseudoTooltip"
+};
+</script>
+
+<style lang="scss" scoped>
+.preudotooltip {
+  position: relative;
+  .tooltip-area {
+    position: absolute;
+    display: none;
+  }
+  &:hover .tooltip-area {
+    display: block;
+  }
+}
+</style>
--- src/pages/sandbox01/Sandbox01.vue
+++ src/pages/sandbox01/Sandbox01.vue
@@ -1,24 +1,45 @@
 <template>
   <div>
+    クリックしたらなんか出る、けど閉じれません。
     <Comp01/>
-    
+
     <Comp01/>
-    
+
     <Comp01/>
+
+    <hr>
+    <div>
+      <PseudoTooltip>
+        ここにマウスをあててみる
+        <div
+          slot="tooltip-content"
+          style="width:200px;height:200px;background-color:rgb(150,230,150)"
+        >お名前は
+          <input type="text" v-model="yourName"/>
+        </div>
+      </PseudoTooltip>
+    </div>
+    入力した名前：<input type="text" :value="yourName" readonly>
   </div>
 </template>
 
 <script>
-import Comp01 from './Comp01'
+import Comp01 from "./Comp01";
+import PseudoTooltip from "./PseudoTooltip";
 
 export default {
-  name:'sandbox01',
-  components:{
+  name: "sandbox01",
+  data(){
+    return {
+      yourName:""
+    }
+  },
+  components: {
     Comp01,
+    PseudoTooltip
   }
-}
+};
 </script>
 
 <style lang="scss" scoped>
-
 </style>
